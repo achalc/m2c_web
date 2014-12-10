eb2ffc0b2a97f3a6192634a376b2a1448559e6b2
Achal Channarasappa <achannar@andrew.cmu.edu>
2014-12-09 21:25:37 -0500

    added views dir

---
+++ app.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var dbRoutes = require('./routes/routes.js'); 
+// var foundation = require('foundation');
+var morgan = require('morgan');
+var app = express();
+
+// Set the views directory
+app.set('views', __dirname + '/views');
+
+// Define the view (templating) engine
+app.set('view engine', 'ejs');
+app.use(morgan('tiny'));	// Log requests
+
+// Routes
+app.get('/', dbRoutes.index);
+app.get('/login', dbRoutes.login);
+app.get('/lighting', dbRoutes.light);
+app.get('/log', dbRoutes.log);
+app.get('/send', dbRoutes.send);
+app.get('/:collection/:operation', dbRoutes.mongo);
+app.get('/test', dbRoutes.test);
+
+// Handle static files
+app.use(express.static(__dirname + '/public'));
+  
+// Catch any routes not already handed with an error message
+app.use(dbRoutes.errorMessage);
+
+//require the Twilio module and create a REST client 
+
+
+ 
+// client.messages.create({ 
+// 	to: "4124910128", 
+// 	from: "+14123019165", 
+// 	body: "Testing 123",   
+// }, function(err, message) { 
+// 	console.log(message.sid); 
+// });
+
+
+
+/*
+ * OpenShift will provide environment variables indicating the IP 
+ * address and PORT to use.  If those variables are not available
+ * (e.g. when you are testing the application on your laptop) then
+ * use default values of localhost (127.0.0.1) and 33333 (arbitrary).
+ */
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 50000;
+
+//  Start listening on the specific IP and PORT
+app.listen(port, ipaddress, function() {
+  console.log('%s: Node server started on %s:%d ...',
+               Date(Date.now() ), ipaddress, port);
+               });
