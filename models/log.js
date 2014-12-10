76e82528106811ac73f60f53c259f82bec41c8e4
Achal Channarasappa <achannar@andrew.cmu.edu>
2014-12-09 23:29:50 -0500

    changed openshift mongo creds

--- models/log.js
+++ models/log.js
@@ -2,15 +2,13 @@ var util = require("util");
 var mongoClient = require('mongodb').MongoClient;
 
 // default to a 'localhost' configuration:
-var connection_string = '127.0.0.1:27017/achannar';
+// var connection_string = '127.0.0.1:27017/achannar';
 // if OPENSHIFT env variables are present, use the available connection info:
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+var connection_string = "mongodb://" + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
   process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
   process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
   process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
   process.env.OPENSHIFT_APP_NAME;
-};
 
 
 var url = connection_string;
