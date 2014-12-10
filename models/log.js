fc44a7938fc660d6782ebca49e09e7da9cfdabf4
Achal Channarasappa <achannar@andrew.cmu.edu>
2014-12-09 23:25:04 -0500

    made mongo config changes

--- models/log.js
+++ models/log.js
@@ -1,7 +1,19 @@
 var util = require("util");
 var mongoClient = require('mongodb').MongoClient;
 
-var url = 'mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/';
+// default to a 'localhost' configuration:
+var connection_string = '127.0.0.1:27017/achannar';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+};
+
+
+var url = connection_string;
 var mongoDB; // The connected database
 // Use connect method to connect to the Server
 mongoClient.connect(url, function(err, db) {
--- server.js
+++ server.js
@@ -5,28 +5,30 @@ var fs      = require('fs');
 var dbRoutes = require('./routes/routes.js'); 
 var morgan = require('morgan');
 var app = express();
+var self = this;
+self.app = express();
 
 // Set the views directory
-app.set('views', __dirname + '/views');
+self.app.set('views', __dirname + '/views');
 
 // Define the view (templating) engine
-app.set('view engine', 'ejs');
-app.use(morgan('tiny'));    // Log requests
+self.app.set('view engine', 'ejs');
+self.app.use(morgan('tiny'));    // Log requests
 
 // Routes
-app.get('/', dbRoutes.index);
-app.get('/login', dbRoutes.login);
-app.get('/lighting', dbRoutes.light);
-app.get('/log', dbRoutes.log);
-app.get('/send', dbRoutes.send);
-app.get('/:collection/:operation', dbRoutes.mongo);
-app.get('/test', dbRoutes.test);
+self.app.get('/', dbRoutes.index);
+self.app.get('/login', dbRoutes.login);
+self.app.get('/lighting', dbRoutes.light);
+self.app.get('/log', dbRoutes.log);
+self.app.get('/send', dbRoutes.send);
+self.app.get('/:collection/:operation', dbRoutes.mongo);
+self.app.get('/test', dbRoutes.test);
 
 // Handle static files
-app.use(express.static(__dirname + '/public'));
+self.app.use(express.static(__dirname + '/public'));
   
 // Catch any routes not already handed with an error message
-app.use(dbRoutes.errorMessage);
+self.app.use(dbRoutes.errorMessage);
 
 
 
@@ -34,7 +36,7 @@ var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 50000;
 
 //  Start listening on the specific IP and PORT
-app.listen(port, ipaddress, function() {
+self.app.listen(port, ipaddress, function() {
   console.log('%s: Node server started on %s:%d ...',
                Date(Date.now() ), ipaddress, port);
                });
